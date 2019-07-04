285d8d86736fbeae2f7c2d5b2529de564a63b91f
King Pra <kingi.pra@gmail.com>
2019-07-04 16:30:22 -0500

    initial commit

---
+++ src/main/java/com/groupProject/RestControllerDknsApplication.java
@@ -0,0 +1,13 @@
+package com.groupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestControllerDknsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestControllerDknsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupProject/RestControllerDknsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.groupProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestControllerDknsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
